2d518198926323069026d20b7bec7822d4bfd1c5
Sarahi <al060884@uacam.mx>
2021-11-17 13:08:25 -0600

    se termino

--- views/Acercade.php
+++ views/Acercade.php
@@ -1,90 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Acerca de</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
    <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+   <link rel="stylesheet" href="css/style_acerca.css">
 
-</head>
 
 <body>
     <!--inicio del encabezado con algo sad-->
 
-   
-
-
-      
+    <div class="container">
     <div class="row">
-    <div class="col s3 pink lighten-5"> 
-        <div class="row">
-            <div class="col s12 ">
-              <div class="card">
-                <div class="card-image">
-                  <img src="Img/music.jpg">
-                  <span class="card-title black-text">Musica</span>
+
+        <div class="col s4">
+            <!-- Promo Content 1 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Visión</span>
+                    <p>Hacer que los maestros de música manejen de forma correcta a sus alumnos
+                      así como tambien puedan disfrutar de la enseñanza. 
+                    </p>
                 </div>
-                <div class="card-content ">
-                  <p>El concepto de música ha ido evolucionando desde su origen en la Antigua Grecia, 
-                      en que se reunía sin distinción a la poesía, la música y la danza como arte unitario. 
-                      Desde hace varias décadas se ha vuelto más compleja la definición de qué es y qué no es la música,
-                    ya que destacados compositores en el marco de diversas experiencias artísticas fronterizas han realizado obras que, 
-                    si bien podrían considerarse musicales, expanden los límites de la definición de este arte</p>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
+
                 </div>
-                <div class="card-action blue-text">
-                  <a href="https://es.wikipedia.org/wiki/Música">Mas Información</a>
-                </div>
-              </div>
             </div>
-          </div>
-    </div>
-            
-    <div class="col s9">
-        <div class="card  cyan lighten-5">
-            <div class="card-content center">
-                <h3>Acerca de la Musica</h3>
-                <h6>
-                    En lo personal la musica, es lo mejor que puede haber, esta te llena de sentimientos y muchas emociones.
-                </h6>
-
-                <div class="carousel carousel-slider" id="demo-carousel-indicators" data-indicators="true">
-                    <a class="carousel-item" href="#one!"><img src="Img/Angela.jpg"></a>
-                    <a class="carousel-item" href="#two!"><img src="Img/Tini.jpg"></a>
-                    <a class="carousel-item" href="#three!"><img src="Img/Carlito.jpg"></a>
-                    <a class="carousel-item" href="#four!"><img src="Img/alejandrito.jpg"></a>
-                    <a class="carousel-item" href="#five!"><img src="Img/morat.jpeg"></a>
+        </div>
+        <div class="col s4">
+            <!-- Promo Content 2 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Atención</span>
+                    <p>Brindar el servicio a todos los maestros o alumnos, para aprender
+                      de forma rapida y sencilla.
+                    </p>
                 </div>
-                  <h4>
-                   Carousel de personas que escucho
-                  </h4>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
 
                 </div>
+            </div>
+        </div>
+        <div class="col s4">
+            <!-- Promo Content 3 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Misión</span>
+                    <p>Proporcionar un servicio de organización para los maestros, generando así
+                      la organización de sus alumnos.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
 
-           
-          </div>
+                </div>
+            </div>
+        </div>
 
     </div>
-    
-  
 </div>
-    <!--Pie de pagina-->
- 
- 
-   
-
-    <script>
-        $(document).ready(function(){
-         $('#demo-carousel-indicators').carousel({fullWidth: true});
-        });
-        </script>
-        
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-
 </body>
-</html>
